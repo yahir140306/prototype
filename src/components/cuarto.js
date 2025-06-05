dc98e5846cf077fd16d2dbfd60b4716713888897
yahir140306 <joseyahirhernandez11@gmail.com>
2025-06-04 23:00:31 -0600

    add Cuarto component with dynamic rendering of room details

---
+++ src/components/cuarto.js
@@ -0,0 +1,26 @@
+export const CUARTO = [
+    {
+        name: 'Cuarto',
+        description: 'Un cuarto es un espacio cerrado que se utiliza para dormir, descansar o realizar otras actividades privadas. Puede contener una cama, armarios y otros muebles.',
+        image: '/Images/cuarto1.jpeg',
+        price: 800,
+    },
+    {
+        name: 'Cuarto',
+        description: 'Un cuarto es un espacio cerrado que se utiliza para dormir, descansar o realizar otras actividades privadas. Puede contener una cama, armarios y otros muebles.',
+        image: '/Images/cuarto1.jpeg',
+        price: 800,
+    },
+    {
+        name: 'Cuarto',
+        description: 'Un cuarto es un espacio cerrado que se utiliza para dormir, descansar o realizar otras actividades privadas. Puede contener una cama, armarios y otros muebles.',
+        image: '/Images/cuarto1.jpeg',
+        price: 800,
+    },
+    {
+        name: 'Cuarto',
+        description: 'Un cuarto es un espacio cerrado que se utiliza para dormir, descansar o realizar otras actividades privadas. Puede contener una cama, armarios y otros muebles.',
+        image: '/Images/cuarto1.jpeg',
+        price: 800,
+    },
+]
