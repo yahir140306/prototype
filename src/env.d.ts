3a822fc323dbe532c9197faf89cad7174a7b4b68
yahir140306 <joseyahirhernandez11@gmail.com>
2025-06-08 23:41:56 -0600

    feat: add TypeScript definitions for Supabase environment variables

---
+++ src/env.d.ts
@@ -0,0 +1,10 @@
+/// <referenece types="astro/client" />
+
+interface ImportMetaEnv {
+    readonly PUBLIC_SUPABASE_URL: string;
+    readonly PUBLIC_SUPABASE_ANON_KEY: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
