09a2ee50486d09876cb41f62d92d9d19cd195e1d
yahir140306 <joseyahirhernandez11@gmail.com>
2025-07-20 13:41:33 -0600

    style: format code for better readability in carousel script

--- src/pages/cuarto/script.js
+++ src/pages/cuarto/script.js
@@ -1,13 +1,13 @@
-        const carousel = document.getElementById("carousel");
-        let index = 0;
+const carousel = document.getElementById("carousel");
+let index = 0;
 
-        setInterval(() => {
-          const children = carousel.children;
-          if (children.length === 0) return;
-          index = (index + 1) % children.length;
-          const nextImage = children[index];
-          carousel.scrollTo({
-            left: nextImage.offsetLeft,
-            behavior: "smooth",
-          });
-        }, 4000);
+setInterval(() => {
+  const children = carousel.children;
+  if (children.length === 0) return;
+  index = (index + 1) % children.length;
+  const nextImage = children[index];
+  carousel.scrollTo({
+    left: nextImage.offsetLeft,
+    behavior: "smooth",
+  });
+}, 4000);
